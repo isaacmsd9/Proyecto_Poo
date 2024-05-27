d18e4122651452630455cb6f14020082656fbbc1
Isaac Mendoza <87744074+isaacmsd9@users.noreply.github.com>
2024-05-26 18:43:35 -0600

    Adición de javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"modelo","l":"Alumno"},{"p":"controlador","l":"Controlador"},{"p":"modelo","l":"listadoSemestres"},{"p":"modelo","l":"NumeroCuentaGenerator"},{"p":"vista","l":"Vista"}];updateSearchResults();
