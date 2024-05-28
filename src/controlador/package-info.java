b32e4afa94a6f18c3894298ead32b595540fadc7
Isaac Mendoza <87744074+isaacmsd9@users.noreply.github.com>
2024-05-27 18:40:13 -0600

    Delete src directory

--- src/controlador/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Proporciona las clases necesarias para manejar la lógica de negocio de la aplicación.
- * Incluye la clase principal que ejecuta la aplicación y controla las interacciones entre
- * la vista y el modelo, procesando la entrada del usuario y manejando la ejecución del flujo del programa.
- */
-package controlador;
