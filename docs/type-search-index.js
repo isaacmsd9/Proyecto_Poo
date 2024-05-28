028799e148d76cbd7877103f8489ded49379d638
Isaac Mendoza <87744074+isaacmsd9@users.noreply.github.com>
2024-05-27 18:40:25 -0600

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"modelo","l":"Alumno"},{"p":"controlador","l":"Controlador"},{"p":"modelo","l":"listadoSemestres"},{"p":"modelo","l":"NumeroCuentaGenerator"},{"p":"vista","l":"Vista"}];updateSearchResults();
